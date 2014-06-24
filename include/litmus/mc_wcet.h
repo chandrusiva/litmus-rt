034f81fe0ff3d45909a282c62e9bdd08b28e394a
chandru <chandru.a6@gmail.com>
2014-06-24 16:15:33 +0800

    Adding a separate header for structure holding multiple wcet values..

---
+++ include/litmus/mc_wcet.h
@@ -0,0 +1,18 @@
+#ifndef _MC_WCET_H_
+#define _MC_WCET_H_
+
+/*To include linked list implementation */
+#include <litmus/list_userspace.h>
+
+/*litmus time type */
+typedef unsigned long long lt_t;
+
+/*Declaration of structure which will hold multiple wcet values */
+
+struct exec_times{
+	lt_t wcet_val;
+	struct list_head_u list;
+};
+
+#endif
+
