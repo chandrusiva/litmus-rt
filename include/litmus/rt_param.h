50e238d48d5a5bfae95fdd25d51b0c15b11c5cb8
chandru <chandru.a6@gmail.com>
2014-06-25 02:04:11 +0800

    Adding a struct exec_times variable to the rt_task struct..

--- include/litmus/rt_param.h
+++ include/litmus/rt_param.h
@@ -86,6 +86,8 @@ struct rt_task {
 	task_class_t	cls;
 	budget_policy_t  budget_policy;  /* ignored by pfair */
 	release_policy_t release_policy;
+	/* This is the list structure which contains multiple wcet values */
+	struct exec_times mylist_k;
 };
 
 union np_flag {
