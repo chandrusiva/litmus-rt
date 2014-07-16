e8484be73406997998dd33dc753e4c849fa0d507
chandru <chandru.a6@gmail.com>
2014-07-17 05:48:57 +0800

    step 9 over..

--- litmus/sched_edf_vd.c
+++ litmus/sched_edf_vd.c
@@ -10,6 +10,7 @@
 //#include <litmus/rt_domain.h>
 #include <litmus/jobs.h>
 #include <litmus/budget.h>
+#include <litmus/litmus_proc.h>
 
 //This struct is not typedefd in tutorial
 typedef struct {
@@ -21,18 +22,11 @@ typedef struct {
 //Not defined as static in built-in plugins
 static DEFINE_PER_CPU(edf_vd_domain_t, edf_vd_domains);
 
+static struct domain_proc_info edf_vd_domain_proc_info;
+
 //Used with different names in built-in plugins
 #define cpu_state_for(cpu_id)   (&per_cpu(edf_vd_domains, cpu_id))
 #define local_cpu_state()       (&__get_cpu_var(edf_vd_domains))
-
-/* this helper is called when task `prev` exhausted its budget or when
- * it signaled a job completion */
-static void edf_vd_job_completion(struct task_struct *prev, int budget_exhausted)
-{
-        /* call common helper code to compute the next release time, deadline,
-         * etc. */
-        prepare_for_next_period(prev);
-}
 
 /* Add the task `tsk` to the appropriate queue. Assumes caller holds the ready lock.
  */
@@ -48,6 +42,84 @@ static void edf_vd_requeue(struct task_struct *tsk, edf_vd_domain_t *cpu_state)
                 add_release(&cpu_state->local_queues, tsk);
         }
 }
+
+static void edf_vd_task_new(struct task_struct *tsk, int on_runqueue,
+                          int is_running)
+{
+        unsigned long flags; /* needed to store the IRQ flags */
+        edf_vd_domain_t *state = cpu_state_for(get_partition(tsk));
+        lt_t now;
+
+        TRACE_TASK(tsk, "is a new RT task %llu (on_rq:%d, running:%d)\n",
+                   litmus_clock(), on_runqueue, is_running);
+
+        /* acquire the lock protecting the state and disable interrupts */
+        raw_spin_lock_irqsave(&state->local_queues.ready_lock, flags);
+
+        now = litmus_clock();
+
+        /* the first job exists starting as of right now */
+        release_at(tsk, now);
+
+        if (is_running) {
+                /* if tsk is running, then no other task can be running
+                 * on the local CPU */
+                BUG_ON(state->scheduled != NULL);
+                state->scheduled = tsk;
+        } else if (on_runqueue) {
+                edf_vd_requeue(tsk, state);
+        }
+	
+	if (edf_preemption_needed(&state->local_queues, state->scheduled))
+                           preempt_if_preemptable(state->scheduled, state->cpu);
+        
+	raw_spin_unlock_irqrestore(&state->local_queues.ready_lock, flags);
+}
+
+static void edf_vd_task_exit(struct task_struct *tsk)
+{
+        unsigned long flags; /* needed to store the IRQ flags */
+        edf_vd_domain_t *state = cpu_state_for(get_partition(tsk));
+
+        /* acquire the lock protecting the state and disable interrupts */
+        raw_spin_lock_irqsave(&state->local_queues.ready_lock, flags);
+
+        if (state->scheduled == tsk)
+                state->scheduled = NULL;
+
+        /* For simplicity, we assume here that the task is no longer queued anywhere else. This
+         * is the case when tasks exit by themselves; additional queue management is
+         * is required if tasks are forced out of real-time mode by other tasks. */
+
+        raw_spin_unlock_irqrestore(&state->local_queues.ready_lock, flags);
+}
+
+static int edf_vd_check_for_preemption_on_release(rt_domain_t *local_queues)
+{
+        edf_vd_domain_t *state = container_of(local_queues, edf_vd_domain_t,
+                                                    local_queues);
+
+        /* Because this is a callback from rt_domain_t we already hold
+         * the necessary lock for the ready queue.
+         */
+
+        if (edf_preemption_needed(local_queues, state->scheduled)) {
+                preempt_if_preemptable(state->scheduled, state->cpu);
+                return 1;
+        } else
+                return 0;
+}
+
+/* this helper is called when task `prev` exhausted its budget or when
+ * it signaled a job completion */
+static void edf_vd_job_completion(struct task_struct *prev, int budget_exhausted)
+{
+        /* call common helper code to compute the next release time, deadline,
+         * etc. */
+        prepare_for_next_period(prev);
+}
+
+
 //job_completed is used here. It denotes sleep which is used in builtin plugins
 static struct task_struct* edf_vd_schedule(struct task_struct * prev)
 {
@@ -117,10 +189,84 @@ static struct task_struct* edf_vd_schedule(struct task_struct * prev)
         return next;
 }
 
+/* Called when the state of tsk changes back to TASK_RUNNING.
+ * We need to requeue the task.
+ *
+ * NOTE: if a sporadic task suspended for a long time,
+ * this might actually be an event-driven release of a new job.
+ *
+ */
+static void edf_vd_task_resume(struct task_struct  *tsk)
+{
+        unsigned long flags; /* needed to store the IRQ flags */
+        edf_vd_domain_t *state = cpu_state_for(get_partition(tsk));
+        lt_t now;
+
+        TRACE_TASK(tsk, "wake_up at %llu\n", litmus_clock());
+
+        /* acquire the lock protecting the state and disable interrupts */
+        raw_spin_lock_irqsave(&state->local_queues.ready_lock, flags);
+
+        now = litmus_clock();
+
+        if (is_sporadic(tsk) && is_tardy(tsk, now)) {
+                /* This sporadic task was gone for a "long" time and woke up past
+                 * its deadline. Give it a new budget by triggering a job
+                 * release. */
+                release_at(tsk, now);
+        }
+
+        /* This check is required to avoid races with tasks that resume before
+         * the scheduler "noticed" that it resumed. That is, the wake up may
+         * race with the call to schedule(). */
+        if (state->scheduled != tsk)
+	{
+                edf_vd_requeue(tsk, state);
+		if (edf_preemption_needed(&state->local_queues, state->scheduled))
+                           preempt_if_preemptable(state->scheduled, state->cpu);	
+	}
+
+        raw_spin_unlock_irqrestore(&state->local_queues.ready_lock, flags);
+}
+
 static long edf_vd_admit_task(struct task_struct* tsk)
 {
-	TRACE_TASK(tsk, "Rejected by EDF-VD plugin..\n");
-	return -EINVAL;
+	if (task_cpu(tsk) == get_partition(tsk)) {
+                TRACE_TASK(tsk, "accepted by demo plugin.\n");
+                return 0;
+        } else
+                return -EINVAL;
+}
+
+static long edf_vd_get_domain_proc_info(struct domain_proc_info **ret)
+{
+        *ret = &edf_vd_domain_proc_info;
+        return 0;
+}
+
+static void edf_vd_setup_domain_proc(void)
+{
+        int i, cpu;
+        int num_rt_cpus = num_online_cpus();
+
+        struct cd_mapping *cpu_map, *domain_map;
+
+        memset(&edf_vd_domain_proc_info, sizeof(edf_vd_domain_proc_info), 0);
+        init_domain_proc_info(&edf_vd_domain_proc_info, num_rt_cpus, num_rt_cpus);
+        edf_vd_domain_proc_info.num_cpus = num_rt_cpus;
+        edf_vd_domain_proc_info.num_domains = num_rt_cpus;
+
+        i = 0;
+        for_each_online_cpu(cpu) {
+                cpu_map = &edf_vd_domain_proc_info.cpu_to_domains[i];
+                domain_map = &edf_vd_domain_proc_info.domain_to_cpus[i];
+
+                cpu_map->id = cpu;
+                domain_map->id = i;
+                cpumask_set_cpu(i, cpu_map->mask);
+                cpumask_set_cpu(cpu, domain_map->mask);
+                ++i;
+        }
 }
 
 static long edf_vd_activate_plugin(void)
@@ -136,45 +282,39 @@ static long edf_vd_activate_plugin(void)
    
                    state->cpu = cpu;
                    state->scheduled = NULL;
-                   edf_domain_init(&state->local_queues, NULL, NULL);
+                   edf_domain_init(&state->local_queues, edf_vd_check_for_preemption_on_release, NULL);
             }
-   
-           return 0;
+   		
+	    edf_vd_setup_domain_proc();
+            return 0;
     }
+
+static long edf_vd_deactivate_plugin(void)
+{
+        destroy_domain_proc_info(&edf_vd_domain_proc_info);
+        return 0;
+}
 
 /*	Plugin object	*/
 //cacheline aligned attribute is given in builtin plugins
+//wake_up is being called as resume here..
 static struct sched_plugin edf_vd_plugin __cacheline_aligned_in_smp = {
 	.plugin_name		= "EDF-VD",
-	//.task_new		= psnedf_task_new,
+	.task_new		= edf_vd_task_new,
 	//.complete_job		= complete_job,
-	//.task_exit		= psnedf_task_exit,
+	.task_exit		= edf_vd_task_exit,
 	.schedule		= edf_vd_schedule,
-	//.task_wake_up		= psnedf_task_wake_up,
+	.task_wake_up		= edf_vd_task_resume,
 	//.task_block		= psnedf_task_block,
 	.admit_task		= edf_vd_admit_task,
 	.activate_plugin	= edf_vd_activate_plugin,
-	//.deactivate_plugin	= psnedf_deactivate_plugin,
-	//.get_domain_proc_info	= psnedf_get_domain_proc_info,
+	.deactivate_plugin	= edf_vd_deactivate_plugin,
+	.get_domain_proc_info	= edf_vd_get_domain_proc_info,
 };
 
 
 static int __init init_edf_vd(void)
 {
-	/*
-	int i;
-	*/
-	/* We do not really want to support cpu hotplug, do we? ;)
-	 * However, if we are so crazy to do so,
-	 * we cannot use num_online_cpu()
-	 */
-	/*
-	for (i = 0; i < num_online_cpus(); i++) {
-		psnedf_domain_init(remote_pedf(i),
-				   psnedf_check_resched,
-				   NULL, i);
-	}
-	*/
 	return register_sched_plugin(&edf_vd_plugin);
 }
 
