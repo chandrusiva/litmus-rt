440047f12c28168297d77e25b1db5962885b790c
chandru <chandru.a6@gmail.com>
2014-07-16 22:24:51 +0800

    In step 4..

--- litmus/sched_edf_vd.c
+++ litmus/sched_edf_vd.c
@@ -1,5 +1,25 @@
 #include <litmus/preempt.h>
 #include <litmus/sched_plugin.h>
+#include <linux/percpu.h>
+#include <litmus/litmus.h>
+#include <litmus/edf_common.h>
+//edf_common includes rt_domain header, so uncomment below..
+//#include <litmus/rt_domain.h>
+
+//This struct is not typedefd in tutorial
+typedef struct {
+	rt_domain_t 		local_queues;
+	int          		cpu;
+	struct task_struct* 	scheduled; /* only RT tasks */
+} edf_vd_domain_t;
+
+//Not defined as static in built-in plugins
+static DEFINE_PER_CPU(edf_vd_domain_t, edf_vd_domains);
+
+//Used with different names in built-in plugins
+#define cpu_state_for(cpu_id)   (&per_cpu(edf_vd_domains, cpu_id))
+#define local_cpu_state()       (&__get_cpu_var(edf_vd_domains))
+
 
 static struct task_struct* edf_vd_schedule(struct task_struct * prev)
 {
@@ -11,8 +31,28 @@ static struct task_struct* edf_vd_schedule(struct task_struct * prev)
 
 static long edf_vd_admit_task(struct task_struct* tsk)
 {
+	TRACE_TASK(tsk, "Rejected by EDF-VD plugin..\n");
 	return -EINVAL;
 }
+
+static long edf_vd_activate_plugin(void)
+    {
+            //The following is done in domain_init in built-in plugins
+	    int cpu;
+            edf_vd_domain_t *state;
+    
+            for_each_online_cpu(cpu) {
+                   TRACE("Initializing CPU%d...\n", cpu);
+    
+                   state = cpu_state_for(cpu);
+   
+                   state->cpu = cpu;
+                   state->scheduled = NULL;
+                   edf_domain_init(&state->local_queues, NULL, NULL);
+            }
+   
+           return 0;
+    }
 
 /*	Plugin object	*/
 static struct sched_plugin edf_vd_plugin __cacheline_aligned_in_smp = {
@@ -24,7 +64,7 @@ static struct sched_plugin edf_vd_plugin __cacheline_aligned_in_smp = {
 	//.task_wake_up		= psnedf_task_wake_up,
 	//.task_block		= psnedf_task_block,
 	.admit_task		= edf_vd_admit_task,
-	//.activate_plugin	= psnedf_activate_plugin,
+	.activate_plugin	= edf_vd_activate_plugin,
 	//.deactivate_plugin	= psnedf_deactivate_plugin,
 	//.get_domain_proc_info	= psnedf_get_domain_proc_info,
 };
