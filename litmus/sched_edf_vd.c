26c37987bcb4873b899f72efa6632840f043dbbe
chandru <chandru.a6@gmail.com>
2014-07-17 03:02:53 +0800

    Step 5..

--- litmus/sched_edf_vd.c
+++ litmus/sched_edf_vd.c
@@ -1,3 +1,6 @@
+
+/*This is the plugin implementation for EDF-VD scheduler */
+
 #include <litmus/preempt.h>
 #include <litmus/sched_plugin.h>
 #include <linux/percpu.h>
@@ -5,6 +8,8 @@
 #include <litmus/edf_common.h>
 //edf_common includes rt_domain header, so uncomment below..
 //#include <litmus/rt_domain.h>
+#include <litmus/jobs.h>
+#include <litmus/budget.h>
 
 //This struct is not typedefd in tutorial
 typedef struct {
@@ -20,13 +25,96 @@ static DEFINE_PER_CPU(edf_vd_domain_t, edf_vd_domains);
 #define cpu_state_for(cpu_id)   (&per_cpu(edf_vd_domains, cpu_id))
 #define local_cpu_state()       (&__get_cpu_var(edf_vd_domains))
 
+/* this helper is called when task `prev` exhausted its budget or when
+ * it signaled a job completion */
+static void edf_vd_job_completion(struct task_struct *prev, int budget_exhausted)
+{
+        /* call common helper code to compute the next release time, deadline,
+         * etc. */
+        prepare_for_next_period(prev);
+}
 
+/* Add the task `tsk` to the appropriate queue. Assumes caller holds the ready lock.
+ */
+//rt_domain is passed directly in builtin, cpu entry is passed here.
+static void edf_vd_requeue(struct task_struct *tsk, edf_vd_domain_t *cpu_state)
+{
+        if (is_released(tsk, litmus_clock())) {
+                /* Uses __add_ready() instead of add_ready() because we already
+                 * hold the ready lock. */
+                __add_ready(&cpu_state->local_queues, tsk);
+        } else {
+                /* Uses add_release() because we DON'T have the release lock. */
+                add_release(&cpu_state->local_queues, tsk);
+        }
+}
+//job_completed is used here. It denotes sleep which is used in builtin plugins
 static struct task_struct* edf_vd_schedule(struct task_struct * prev)
 {
-	//This call is mandatory
-	sched_state_task_picked();
-	//NULL means background best effort tasks will be scheduled by linux
-	return NULL;
+	edf_vd_domain_t  *local_state = local_cpu_state();
+
+        /* next == NULL means "schedule background work". */
+        struct task_struct *next = NULL;
+
+        /* prev's task state */
+        int exists, out_of_time, job_completed, self_suspends, preempt, resched;
+
+        raw_spin_lock(&local_state->local_queues.ready_lock);
+
+        BUG_ON(local_state->scheduled && local_state->scheduled != prev);
+        BUG_ON(local_state->scheduled && !is_realtime(prev));
+
+        exists = local_state->scheduled != NULL;
+        self_suspends = exists && !is_running(prev);
+        out_of_time   = exists && budget_enforced(prev)
+                && budget_exhausted(prev);
+        job_completed = exists && is_completed(prev);
+
+        /* preempt is true if task `prev` has lower priority than something on
+         * the ready queue. */
+        preempt = edf_preemption_needed(&local_state->local_queues, prev);
+
+        /* check all conditions that make us reschedule */
+        resched = preempt;
+
+        /* if `prev` suspends, it CANNOT be scheduled anymore => reschedule */
+        if (self_suspends)
+                resched = 1;
+
+        /* also check for (in-)voluntary job completions */
+        if (out_of_time || job_completed) {
+                demo_job_completion(prev, out_of_time);
+                resched = 1;
+        }
+
+        if (resched) {
+                /* First check if the previous task goes back onto the ready
+                 * queue, which it does if it did not self_suspend.
+                 */
+                if (exists && !self_suspends)
+                        edf_vd_requeue(prev, local_state);
+                next = __take_ready(&local_state->local_queues);
+        } else
+                /* No preemption is required. */
+                next = local_state->scheduled;
+
+        local_state->scheduled = next;
+
+
+        if (exists && prev != next)
+                TRACE_TASK(prev, "descheduled.\n");
+        if (next)
+                TRACE_TASK(next, "scheduled.\n");
+
+        /* This mandatory. It triggers a transition in the LITMUS^RT remote
+         * preemption state machine. Call this AFTER the plugin has made a local
+         * scheduling decision.
+         */
+        sched_state_task_picked();
+
+        raw_spin_unlock(&local_state->local_queues.ready_lock);
+
+        return next;
 }
 
 static long edf_vd_admit_task(struct task_struct* tsk)
@@ -55,6 +143,7 @@ static long edf_vd_activate_plugin(void)
     }
 
 /*	Plugin object	*/
+//cacheline aligned attribute is given in builtin plugins
 static struct sched_plugin edf_vd_plugin __cacheline_aligned_in_smp = {
 	.plugin_name		= "EDF-VD",
 	//.task_new		= psnedf_task_new,
