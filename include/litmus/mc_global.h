7b3cbe41f5b76b21f9096c702132a85ce2df1ec9
chandru <chandru.a6@gmail.com>
2014-07-22 01:42:00 +0800

    Editing variable name

--- include/litmus/mc_global.h
+++ include/litmus/mc_global.h
@@ -8,7 +8,7 @@
 extern int sys_cl;
 
 /*This is the flag which will be set when there is a budget overrun*/
-extern int flag;
+extern int budget_flag;
 //This stores the initial sys_cl. This is used to return to the normal mode
 extern int temp_sys_cl;
 
